fc8d805cfc3fce6fa73b70a1005515d845d53424
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-31 10:53:11 +0300

    Add webhook on future

--- index.js
+++ index.js
@@ -3,8 +3,20 @@
 const TelegramBot = require('node-telegram-bot-api');
 const { Token } = require('./config');
 
-// Create a bot that uses 'polling' to fetch new updates
+// Create a bot that uses 'polling' to fetch new updates.
+// It`s for development
 const bot = new TelegramBot(Token, { polling: true });
+// Create a bot that uses 'webhook' to get new updates.
+//It`s for production ========
+// const options = {
+//     webHook: {
+//       port: process.env.PORT
+//     }
+//   };
+//   const url = process.env.APP_URL || 'https://weather-bot-mezgoodle.herokuapp.com:443';
+//   const bot = new TelegramBot(Token, options);
+//   bot.setWebHook(`${url}/bot${Token}`);
+// =============
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
