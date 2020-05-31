ae0f83276b83607c952bc782bc0f1095872643a3
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-31 10:51:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const TelegramBot = require('node-telegram-bot-api');
 const { Token } = require('./config');
 
-console.log(Token);
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(Token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', msg => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Received your message');
+});
