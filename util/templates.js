045aec3f7582754d98dbc5c784929058e7c17ee0
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-07 10:08:09 +0300

    Add converting to template

--- util/templates.js
+++ util/templates.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const {convertTime} = require('./converter');
+
 const emojies = {
   'true': '✔️',
   'false': '❌'
@@ -20,8 +22,8 @@ const launchHTMLTemplate = launch => (
   `🚀<b>${launch.mission_name}</b>
      🆔Rocket name: ${launch.rocket.rocket_name}
       Upcoming: ${emojies[launch.upcoming]}
-     🔥Launch date: <b>${launch.launch_date_local}</b>
-     🕓Last date update: <b>${launch.last_date_update}</b>
+     🔥Launch date: <b>${convertTime(launch.launch_date_unix)}</b>
+     🕓Last date update(local): <b>${launch.last_date_update}</b>
      📚Details: <b>${launch.details}</b>
      🔗Site: <a href="${launch.links.reddit_launch}">link</a>
     `
